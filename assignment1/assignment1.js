d93c0569dea537ecde9b4a91604305cd63a73488
freysypv <freysypv1@gmail.com>
2026-01-28 19:33:39 -0600

    Add files via upload

---
+++ assignment1/assignment1.js
@@ -0,0 +1,32 @@
+
+
+ let character = "Jose";
+ let action = "Running";
+ let object = "The first and bigest fish of the day";
+ let place = "Eagle mauntain lake";
+ let time = "A cold rainy morning";
+
+ const occasion = "log";
+
+ // story creation
+ console.log("---a fisherman nightmare");
+ console.log(character + "was at" + place + "on" + time +".");
+ console.log("All of the suddent" + character + "saw" + object + "and self proclaim that He was the King fishing, As he started" + action + ".");
+ console.log("It turned out to be" + occasion + " that was the hightlight of the day.");
+
+// story 2
+
+ character = "Maria";
+ action = "brewing a fresh pot of coffee";
+ object = "turn on the stove";
+ place = "outdoors kitchen";
+ time = "warm sunny morning";
+ 
+  
+
+
+// story creation
+ console.log("---The best coffe ever---");
+ console.log(character + "was at" + place + "on" + time +".");
+ console.log("as" + character + "clean and " + object + "to start" + action + ".");
+ console.log("Everybody was always happy to start the morning with a cup of coffee and good conversation> i used to sit on a " + occasion + " by the door waiting for Maria to came throw.That was my favorite part of that morning.");
